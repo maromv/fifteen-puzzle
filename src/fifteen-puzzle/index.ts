e47dfa419139102de6c3540d86533fd021c8a17a
Roman Mazhara <roman.mazhara@proton.me>
2026-04-17 04:22:59 +0300

    chore: fifteen-puzzle index

---
+++ src/fifteen-puzzle/index.ts
@@ -0,0 +1,12 @@
+export {
+  BLANK,
+  countInversions,
+  FifteenPuzzleBoard,
+  InvalidGridError,
+  isSolvable,
+  SIZE,
+  ValueOutOfRangeError,
+} from './domain/FifteenPuzzleBoard';
+export { FifteenPuzzleGame } from './application/FifteenPuzzleGame';
+export { FifteenPuzzleTerminalRenderer } from './terminal/FifteenPuzzleTerminalRenderer';
+export { FifteenPuzzleCli } from './terminal/FifteenPuzzleCli';
